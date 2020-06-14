c2363c334ee499136651d7c696a10a2e0c31bb2f
Fernando Severino <f.almeida@xsensors.ai>
2020-06-14 11:52:46 -0400

    Test 2

--- app.js
+++ app.js
@@ -1,18 +1,63 @@
 "use strict";
 
 const app = require("express")();
-app.set("view engine", "pug");
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-const server = require("http").Server(app);
+const connectedMotoboys = {};
+const pendingRaces = [];
+const currentRaces = [];
 
-app.get("/", (req, res) => {
-  res.render("index.pug");
+io.on("connection", (socket) => {
+  const { user_id, user_type } = socket.handshake.query;
+
+  if (user_type === "motoboy") {
+    connectedMotoboys[user_id] = { socketId: socket.id, userId: user_id };
+
+    socket.on("setMotoboyLocation", (msg) => {
+      const data = JSON.parse(msg);
+
+      connectedMotoboys[data.id].location = data.location;
+      connectedMotoboys[data.id].lastTimeOnline = data.lastTimeOnline;
+
+      emitConnectedMotoboys();
+    });
+
+    socket.on("findMotoboy", (msg) => {
+      const data = JSON.parse(msg);
+      pendingRaces.push(data);
+
+      io.emit("pendingRaces", JSON.stringify(pendingRaces));
+    });
+
+    socket.on("disconnectMotoboy", (msg) => {
+      const data = JSON.parse(msg);
+
+      delete connectedMotoboys[data.id];
+
+      io.emit("connnectedMotoboys", JSON.stringify(connectedMotoboys));
+    });
+  }
 });
 
-if (module === require.main) {
-  const PORT = process.env.PORT || 8080;
-  server.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-    console.log("Press Ctrl+C to quit.");
-  });
-}
+const emitConnectedMotoboys = () => {
+  io.emit("connnectedMotoboys", JSON.stringify(connectedMotoboys));
+};
+
+setInterval(function () {
+  const currentTime = new Date();
+
+  for (const motoBoy in connectedMotoboys) {
+    if (
+      currentTime.getTime() - connectedMotoboys[motoBoy].lastTimeOnline >
+      3600000 * 5 // if lastTimeOnline >= 1 hours - Remove user
+    )
+      delete connectedMotoboys[motoBoy];
+  }
+
+  //if (Object.keys(connectedMotoboys).length > 0) emitConnectedMotoboys();
+}, 3600000);
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
